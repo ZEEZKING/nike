43ee79d5a34b68a968fcaaac89ab0734777e5032
ZEEZKING <abdulazeezadekoya17@gmail.com>
2024-03-05 16:18:22 +0100

    FirstCommit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+
+export{
+   Hero,
+   PopularProducts,
+   SuperQuality,
+   Services,
+   Footer,
+   Subscribe,
+   CustomerReviews,
+   SpecialOffer 
+}
